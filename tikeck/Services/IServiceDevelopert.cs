3803f4de0cd25610842fff73f4fdc27b37ce2679
felix diaz <felixcamposlu@gmail.com>
2025-04-26 14:54:54 -0500

    added models enum and systemTickect class

---
+++ tikeck/Services/IServiceDevelopert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tikeck.Models;
+
+namespace tikeck.Services
+{
+   public interface IServiceTickect
+   {
+        void AddTticket(Developer developer);
+        void ShowTickect();
+
+        void ShowTicketById();
+
+
+   }
+}
